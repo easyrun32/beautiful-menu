71ac481234a15aab0f6c18de233ca65011d603d6
bryan aviles <easyrun32@gmail.com>
2021-04-20 11:03:00 -0400

    deploy

--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,10 @@ import express from "express";
 // https://dev.to/hte305/simple-deploy-typescript-application-to-heroku-5b6g
 const app = express();
 const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.get("/typescript", (req, res) => res.send("Express + TypeScript Server"));
+app.use("*", (req, res) => {
+  res.send("<h1>Welcome to your simple server! Awesome right</h1>");
+});
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
