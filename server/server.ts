8d6cd31b2d42e2bf55850e2384b80585012cda0e
bryan aviles <easyrun32@gmail.com>
2021-04-20 11:08:06 -0400

    try again

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,9 @@
 import express from "express";
-// https://dev.to/hte305/simple-deploy-typescript-application-to-heroku-5b6g
 const app = express();
-const PORT = 8000;
-app.get("/typescript", (req, res) => res.send("Express + TypeScript Server"));
+const PORT: string | number = process.env.PORT || 5000;
+
 app.use("*", (req, res) => {
   res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-});
+
+app.listen(PORT, () => console.log(`hosting @${PORT}`));
